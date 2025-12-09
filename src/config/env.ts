b80566fdabab314e7e6bcac0140ad1aaecf7db0e
Ago Ben <jehovabalan@gmail.com>
2025-12-08 23:28:29 -0500

    updated

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import type { AppEnv } from './types.ts';
 
-export const env: AppEnv = {
+  export const env: AppEnv = {
   baseUrl: process.env.BASE_URL ?? 'http://localhost:3000',
   apiBaseUrl: process.env.API_BASE_URL ?? 'http://localhost:3001',
   adminEmail: process.env.ADMIN_EMAIL ?? 'admin@example.test',
